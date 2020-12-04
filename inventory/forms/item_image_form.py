44afec962fac796389795af817dc01eab641c464
Betty Blaize <bethlakshmi@gmail.com>
2020-12-04 17:31:27 -0500

    Another quick trick - showing when the item is already linked to something.

--- inventory/forms/item_image_form.py
+++ inventory/forms/item_image_form.py
@@ -18,9 +18,14 @@ from inventory.default_form_text import item_image_help
 
 class MultiImageField(ModelMultipleChoiceField):
     def label_from_instance(self, obj):
+        other_links = "No Item Links"
         options = {'size': (100, 100), 'crop': False}
         thumb_url = get_thumbnailer(obj).get_thumbnail(options).url
-        return mark_safe("<img src='%s'/>" % thumb_url)
+        if obj.itemimage_set.exists():
+            other_links = "Linked to:  "
+            for link in obj.itemimage_set.all():
+                other_links = "%s %s;" % (other_links, link.item.title)
+        return mark_safe("<img src='%s' title='%s'/>" % (thumb_url, other_links))
 
 class ItemImageForm(Form):
     required_css_class = 'required'
