6aacbf8b120f47698d81487811f44114fc479cfe
Betty Blaize <bethlakshmi@gmail.com>
2020-12-06 15:16:00 -0500

    one more pepdiff + refactor for error on file upload (conjectural)

--- inventory/forms/default_form_text.py
+++ inventory/forms/default_form_text.py
@@ -2,7 +2,7 @@ item_image_help = {
     'current_images': '''This is the list of all images in the sytem.  Images
      that are currently linked with this item are highlighted in blue.  To add
      or remove the link between the image and the item click the image
-     thumbnail.Unlinking an image from this item will NOT delete the image 
+     thumbnail.Unlinking an image from this item will NOT delete the image
      from the system.''',
     'new_images': '''Upload 1 or many images from your computer.  Select
      many images using CONTROL or SHIFT in the file browser.  Adding images
