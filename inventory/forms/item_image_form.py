3601006431a8a2dc0b31875eedbbac634be260e1
Betty Blaize <bethlakshmi@gmail.com>
2020-12-07 13:36:46 -0500

    set up inheritance for image upload

--- inventory/forms/item_image_form.py
+++ inventory/forms/item_image_form.py
@@ -1,15 +1,11 @@
-from django.forms import (
-    ClearableFileInput,
-    ImageField,
-    Form,
-    ModelMultipleChoiceField,
-)
+from django.forms import ModelMultipleChoiceField
 from django.forms.widgets import CheckboxSelectMultiple
-from inventory.models import UserMessage
 from django.utils.safestring import mark_safe
-from filer.models import Image
 from easy_thumbnails.files import get_thumbnailer
 from inventory.forms.default_form_text import item_image_help
+from inventory.forms import ImageUploadForm
+from filer.models import Image
+from inventory.models import UserMessage
 
 
 class MultiImageField(ModelMultipleChoiceField):
@@ -25,7 +21,7 @@ class MultiImageField(ModelMultipleChoiceField):
             "<img src='%s' title='%s'/>" % (thumb_url, other_links))
 
 
-class ItemImageForm(Form):
+class ItemImageForm(ImageUploadForm):
     required_css_class = 'required'
     error_css_class = 'error'
 
@@ -39,14 +35,4 @@ class ItemImageForm(Form):
                 defaults={
                     'summary': "Current Image Help text",
                     'description': item_image_help['current_images']}
-                )[0].description)
-    new_images = ImageField(
-        widget=ClearableFileInput(attrs={'multiple': True}),
-        required=False,
-        help_text=UserMessage.objects.get_or_create(
-                view="ItemImageForm",
-                code="NEW_IMAGE_INSTRUCTIONS",
-                defaults={
-                    'summary': "New Image Help text",
-                    'description': item_image_help['new_images']}
                 )[0].description)
