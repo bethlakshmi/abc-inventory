d9bb5a3e89175210d0307768cf22f5544d7fbee5
Betty Blaize <bethlakshmi@gmail.com>
2020-12-08 21:15:03 -0500

    undo dumb hack

--- inventory/forms/item_image_form.py
+++ inventory/forms/item_image_form.py
@@ -5,6 +5,7 @@ from django.forms import (
     ModelMultipleChoiceField,
 )
 from django.forms.widgets import CheckboxSelectMultiple
+from inventory.models import UserMessage
 from django.utils.safestring import mark_safe
 from filer.models import Image
 from easy_thumbnails.files import get_thumbnailer
@@ -28,4 +29,24 @@ class ItemImageForm(Form):
     required_css_class = 'required'
     error_css_class = 'error'
 
-
+    current_images = MultiImageField(
+        widget=CheckboxSelectMultiple(attrs={'style': "display: none;"}),
+        queryset=Image.objects.all(),
+        required=False,
+        help_text=UserMessage.objects.get_or_create(
+                view="ItemImageForm",
+                code="CURRENT_IMAGE_INSTRUCTIONS",
+                defaults={
+                    'summary': "Current Image Help text",
+                    'description': item_image_help['current_images']}
+                )[0].description)
+    new_images = ImageField(
+        widget=ClearableFileInput(attrs={'multiple': True}),
+        required=False,
+        help_text=UserMessage.objects.get_or_create(
+                view="ItemImageForm",
+                code="NEW_IMAGE_INSTRUCTIONS",
+                defaults={
+                    'summary': "New Image Help text",
+                    'description': item_image_help['new_images']}
+                )[0].description)
