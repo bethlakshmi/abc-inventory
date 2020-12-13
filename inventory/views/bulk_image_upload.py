7eaf9923bd458720f170f4f23b56b077e854613e
Betty Blaize <bethlakshmi@gmail.com>
2020-12-12 23:37:05 -0500

    fix layout without breaking dynamic dropdown

--- inventory/views/bulk_image_upload.py
+++ inventory/views/bulk_image_upload.py
@@ -12,7 +12,7 @@ from easy_thumbnails.files import get_thumbnailer
 
 class BulkImageUpload(GenericWizard):
     filer_images = []
-    template = 'inventory/image_upload_wizard.tmpl'
+    template = 'inventory/generic_wizard.tmpl'
     page_title = 'Image Upload'
     first_title = 'Select Images to Upload'
     second_title = 'Connect Images to Items'
@@ -36,13 +36,12 @@ class BulkImageUpload(GenericWizard):
     def finish_valid_form(self, request):
         files = request.FILES.getlist('new_images')
         self.links = 0
-        self.num_files = len(files)
         if len(files) > 0:
+            self.num_files = len(files)
             self.filer_images = upload_and_attach(
                 files,
                 request.user)
-        elif self.forms[0].__class__.__name__ == "ImageAssociateMetaForm":
-            self.num_files = self.forms[0].cleaned_data['association_count']
+        elif self.forms[0].__class__.__name__ == "ImageAssociateForm":
             for form in self.forms:
                 if form.__class__.__name__ != "ImageAssociateMetaForm" and (
                         form.cleaned_data['item']):
@@ -65,11 +64,14 @@ class BulkImageUpload(GenericWizard):
                 meta_form = ImageAssociateMetaForm(POST)
                 if not meta_form.is_valid():
                     return []
-                forms = [meta_form]
-                for i in range(0,
-                               meta_form.cleaned_data['association_count']+1):
-                    association_form = form(POST, prefix=str(i))
+                self.num_files = meta_form.cleaned_data['association_count']
+                forms = []
+                for i in range(0, self.num_files):
+                    association_form = form(POST,
+                                            prefix=str(i),
+                                            label_suffix='')
                     forms += [association_form]
+                forms += [meta_form]
                 return forms
         else:
             if str(form().__class__.__name__) == "ImageUploadForm":
@@ -79,7 +81,8 @@ class BulkImageUpload(GenericWizard):
                 association_num = 0
                 for image in self.filer_images:
                     association_form = form(initial={'filer_image': image},
-                                            prefix=str(association_num))
+                                            prefix=str(association_num),
+                                            label_suffix='')
                     thumb_url = get_thumbnailer(image).get_thumbnail(
                         self.options).url
                     association_form.fields['item'].label = mark_safe(
@@ -87,6 +90,6 @@ class BulkImageUpload(GenericWizard):
                     forms += [association_form]
                     association_num = association_num + 1
                 forms += [ImageAssociateMetaForm(
-                    initial={'association_count': association_num - 1})]
+                    initial={'association_count': association_num})]
                 return forms
 
