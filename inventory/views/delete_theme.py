0762aa16a34c3f1a54b6dba291c9cba81a2ea31b
Betty Blaize <bethlakshmi@gmail.com>
2021-02-16 10:11:39 -0500

    code review - streamline code

--- inventory/views/delete_theme.py
+++ inventory/views/delete_theme.py
@@ -21,6 +21,17 @@ class DeleteTheme(View):
         version_id = kwargs.get("version_id")
         self.style_version = get_object_or_404(StyleVersion, id=version_id)
 
+    def error_alert(self, request, error_code):
+        msg = UserMessage.objects.get_or_create(
+            view=self.__class__.__name__,
+            code=error_code,
+            defaults={
+                'summary': user_messages[error_code]['summary'],
+                'description': user_messages[error_code]['description']})
+        messages.error(
+            request,
+            msg[0].description + "  TARGET: " + str(self.style_version))
+
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(DeleteTheme, self).dispatch(*args, **kwargs)
@@ -30,29 +41,11 @@ class DeleteTheme(View):
         theme_display = "%s"
         self.groundwork(request, args, kwargs)
         if StyleVersion.objects.all().count() == 1:
-            error_code = "LAST_THEME"
-            msg = UserMessage.objects.get_or_create(
-                view=self.__class__.__name__,
-                code=error_code,
-                defaults={
-                    'summary': user_messages[error_code]['summary'],
-                    'description': user_messages[error_code]['description']})
-            messages.error(
-                request,
-                msg[0].description + "  TARGET: " + str(self.style_version))
+            self.error_alert(request, "LAST_THEME")
             theme_display = "%s?error_id=" + str(self.style_version.pk)
         elif self.style_version.currently_live or (
                 self.style_version.currently_test):
-            error_code = "CURRENTLY_ACTIVE"
-            msg = UserMessage.objects.get_or_create(
-                view=self.__class__.__name__,
-                code=error_code,
-                defaults={
-                    'summary': user_messages[error_code]['summary'],
-                    'description': user_messages[error_code]['description']})
-            messages.error(
-                request,
-                msg[0].description + "  TARGET: " + str(self.style_version))
+            self.error_alert(request, "CURRENTLY_ACTIVE")
             theme_display = "%s?error_id=" + str(self.style_version.pk)
         else:
             self.style_version.delete()
