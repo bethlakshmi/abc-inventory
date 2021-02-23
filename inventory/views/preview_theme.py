fa23ac1dee0e40faf8b60f17c176552755dea99f
Betty Blaize <bethlakshmi@gmail.com>
2021-02-22 23:49:58 -0500

    cleanup

--- inventory/views/preview_theme.py
+++ inventory/views/preview_theme.py
@@ -4,7 +4,6 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.http import Http404
 from django.shortcuts import get_object_or_404
 from inventory.models import (
     StyleVersion,
