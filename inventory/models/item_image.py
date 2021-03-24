af1da488ea2150547f8716694af32ff52a1a89a1
Betty Blaize <bethlakshmi@gmail.com>
2021-03-24 00:02:01 -0400

    Store data, separate on list

--- inventory/models/item_image.py
+++ inventory/models/item_image.py
@@ -1,4 +1,5 @@
 from django.db.models import (
+    BooleanField,
     CASCADE,
     ForeignKey,
     Model,
@@ -16,5 +17,20 @@ class ItemImage(Model):
         on_delete=CASCADE,
         null=True)
 
+    main_image = BooleanField(default=False)
+
     class Meta:
         app_label = "inventory"
+
+    def save(self, *args, **kwargs):
+        if self.main_image:
+            # select any other main items for this image
+            qs = type(self).objects.filter(main_image=True,
+                                           item=self.item)
+            # except self (if self already exists)
+            if self.pk:
+                qs = qs.exclude(pk=self.pk)
+            # and deactive them
+            qs.update(main_image=False)
+
+        super(ItemImage, self).save(*args, **kwargs)
